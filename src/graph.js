0c938f311e04d9cb4ad2a3756820e7064f0ba388
MarcoCruz <marco123xxz@gmail.com>
2024-07-06 17:55:42 -0400

    feat: added Floyd and Dijkstra

--- src/graph.js
+++ src/graph.js
@@ -1,36 +1,54 @@
 import Vertex from "./graphs/Vertex";
 import bfs  from './graphs/paths/Bfs'
+import GraphFloyd from "./graphs/paths/Floyd";
+import GraphDijkstra from "./graphs/paths/Dijkstra";
 
 export function initGraph() {
-    const v1 = new Vertex('v1');
-    const v2 = new Vertex('v2');
-    const v3 = new Vertex('v3');
-    const v4 = new Vertex('v4');
-    const v5 = new Vertex('v5');
-    const v6 = new Vertex('v6');
-    const v7 = new Vertex('v7');
-    const e1 =  v1.addNeighbor(v2);
-    const e2 = v1.addNeighbor(v3);
-    const e3 = v1.addNeighbor(v4);
-    const e4 =  v3.addNeighbor(v7);
-    const e5 =  v4.addNeighbor(v7);
-    const e6 = v5.addNeighbor(v2);
-    const e7 = v5.addNeighbor(v1);
-    const e8 = v5.addNeighbor(v6);
-    const e9 = v7.addNeighbor(v1);
-    const e10 = v6.addNeighbor(v3);
-    const e11 = v6.addNeighbor(v4);
-    const e12 = v6.addNeighbor(v7);
-    // v1.addNeighbor(v2);
-    // v1.addNeighbor(v3);
-    // v1.addNeighbor(v4);
-    // v2.addNeighbor(v3);
-    // v3.addNeighbor(v4);
-    // v3.addNeighbor(v2);
-    // v3.addNeighbor(v5);
-    // v5.addNeighbor(v3);
-    // v5.addNeighbor(v2);
-    // run dfs
-    return {vertex : [v1, v2, v3, v4, v5,v6 , v7], edges: [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12]}
+    const graph1 = new GraphFloyd();
+    const graph2 = new GraphDijkstra();
+
+    const v1 = graph1.addVertex('v1');
+    const v2 = graph1.addVertex('v2');
+    const v3 = graph1.addVertex('v3');
+    const v4 = graph1.addVertex('v4');
+    const v5 = graph1.addVertex('v5');
+    const v6 = graph1.addVertex('v6');
+    const v7 = graph1.addVertex('v7');
+
+    graph1.addEdge('v1', 'v3', 2);
+    graph1.addEdge('v1', 'v2', 1);
+    graph1.addEdge('v1', 'v3', 16);
+    graph1.addEdge('v2', 'v6', 20);
+    graph1.addEdge('v2', 'v4', 22);
+    graph1.addEdge('v3', 'v4', 11);
+    graph1.addEdge('v3', 'v5', 3);
+    graph1.addEdge('v4', 'v6', 7);
+    graph1.addEdge('v5', 'v6', 7);
+
+    console.log("Ruta corta utilizando Floyd: ");
+    graph1.floyd(); 
+
+    /*
+    const graph2 = new Graph2();
+
+    const v1 = graph1.addVertex('v1');
+    const v2 = graph1.addVertex('v2');
+    const v3 = graph1.addVertex('v3');
+    const v4 = graph1.addVertex('v4');
+    const v5 = graph1.addVertex('v5');
+    const v6 = graph1.addVertex('v6');
+    const v7 = graph1.addVertex('v7');
+
+    graph2.addEdge('v1', 'v2', 8);
+    graph2.addEdge('v1', 'v3', 8);
+    graph2.addEdge('v1', 'v6', 7);
+    graph2.addEdge('v2', 'v3', 9);
+    graph2.addEdge('v2', 'v4', 10);
+    graph2.addEdge('v3', 'v4', 11);
+    graph2.addEdge('v3', 'v6', 12);
+    graph2.addEdge('v4', 'v5', 8);
+    console.log("Ruta corta utilizando Dijkstra: ");
+    graph.dijkstra();
+    */
 }
 initGraph();
